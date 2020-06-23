2a0ef885d3c3f1ceadbd6afef491b918e0e5c842
Dave <dave@imagetaylor.com>
2020-06-23 14:13:55 +1000

    Update bestlan.py

--- bestlan.py
+++ bestlan.py
@@ -3,7 +3,7 @@ def bestlanguage (mylang):
         ("Python","You Rock! Big guy!"),
         ("microPython","You also rock, little guy!"),
         ("Java","So that's how you want to spend your life huh?"),
-        ("C","Did you miss the memo?"),
+        ("C","Did you miss the memo? Also, is it you that keeps leaving all this garbage around?"),
         ("C++","It's ok to be a masochist, as long as you are efficient. Now take out your garbage!"),
         ("Go","You're alright man *chuck*"),
         ("Cobol","Take a seat, you are looking a little pale old guy."),
