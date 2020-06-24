e8303e8fce7fa08266461a06e5e3e9c4a7511b70
Dave <dave@imagetaylor.com>
2020-06-24 13:04:17 +1000

    Update bestlan.py
    Ok fine I can add php

--- bestlan.py
+++ bestlan.py
@@ -7,13 +7,14 @@ def bestlanguage (mylang):
         ("Java","So that's how you want to spend your life huh?"),
         ("C","Did you miss the memo? Also, is it you that keeps leaving all this garbage around?"),
         ("C++","It's ok to be a masochist, as long as you are efficient. Now take out your garbage!"),
+        ("PHP","Weeeeeeeeeeeeeeee, web guy huh?"),
         ("Go","You're alright man *chuck*"),
         ("Cobol","Take a seat, you are looking a little pale old guy."),
         ("Pascal","Dear god, WHY?"))
     for lang in langlist:
         if mylang.lower() == lang[0].lower():
             found =1
-            print ("You like %s %s" % lang)
+            print ("You like %s, %s" % lang)
             break
     if found == 0:
         print ("You like %s, never heard of it" % mylang)
